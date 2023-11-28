c334f106afb6c30ae6ea6019c3a73cf2836acd33
lakshmmim <muddarla.sree@gmail.com>
2023-11-28 10:15:18 +0530

    firstcommit

---
+++ variables.js
@@ -0,0 +1,38 @@
+let x=5
+let y=10
+let z=x+y
+console.log(z) // simple additon
+
+let username= "lakshmmmi"  // best practice is doule collans
+console.log(username)
+
+
+/* doule line commens
+--------------*/
+// one variable allocate many times it throws errors
+
+let a=5
+console.log(a)  //executed
+
+/*let a =6
+console.log(a)  //throws error a vriable allocation 2 times 
+//Identifier 'a' has already been declared*/
+
+//variables rules
+/*1. variables name have nums(charecters() but not in firatposition
+      ex:username1
+2. special nums used $ and _ it can be used any where
+    ex:$username, user_name
+3. if variable namae is long  we have 2 rules
+     1. user_name(snake casing rule)
+     2. userName(camel casing rule)
+4. variable name is long dont use . in between vname
+  ex: user.name (it is wrong)
+  */
+ let user_name1="lakshmmi"
+ let passWord="sree"
+ let $otp= 1234
+ //let user.Name2="see"
+ console.log(user_name1)
+ console.log(passWord)
+ console.log($otp)
